67015f3177fd734a28ce43f213287c3cf9e941f7
whitfin <nathan.whitfield@autodesk.com>
2020-01-24 19:23:08 -0500

    started creating the search UI and updated the etl lambda to set active while mapping as a boolean instead of a number

---
+++ user-search-ui/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <UserSearch msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import UserSearch from '@/components/UserSearch.vue'
+
+export default {
+  name: 'home',
+  components: {
+    UserSearch
+  }
+}
+</script>
