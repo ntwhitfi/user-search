50a80994b9a9596faf102dc9fac081ebb8fe2399
whitfin <nathan.whitfield@autodesk.com>
2020-02-05 18:26:02 -0500

    updated the UI to provide user login

---
+++ user-search-ui/src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <UserLogin/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import UserLogin from '@/components/UserLogin.vue'
+
+export default {
+  name: 'home',
+  components: {
+    UserLogin
+  }
+}
+</script>
