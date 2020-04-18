d0cc45d30c897c0957594cf9c2f42408783b38a1
whitfin <nathan.whitfield@autodesk.com>
2020-04-18 18:26:33 -0400

    Updated the deployment to deploy the application to us-east-1 and us-west-1

--- user-search-ui/build.sh
+++ user-search-ui/build.sh
@@ -4,8 +4,8 @@ echo building $ENVIRONMENT environment
 npm install
 npm run build
 
-aws s3 sync --delete --acl bucket-owner-full-control dist/ "s3://${BUCKET_NAME}"
-aws configure set preview.cloudfront true
+aws s3 sync --delete --acl bucket-owner-full-control dist/ "s3://${BUCKET_NAME}" --region ${REGION}
+aws configure set preview.cloudfront true --region ${REGION}
 
-aws cloudfront create-invalidation --distribution-id ${CLOUDFRONT_DISTRIBUTION_ID} --paths '/*'
+aws cloudfront create-invalidation --distribution-id ${CLOUDFRONT_DISTRIBUTION_ID} --paths '/*' --region ${REGION}
 echo Build completed on `date`
