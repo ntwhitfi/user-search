90c3b7960cb3cf815ee17e1d2aba91546715c92b
whitfin <nathan.whitfield@autodesk.com>
2020-04-18 21:20:36 -0400

    updated dynamodb script

--- dynamodb_deployment.py
+++ dynamodb_deployment.py
@@ -1,6 +1,6 @@
 import boto3
 
-dynamodb_client = boto3.client('dynamodb')
+dynamodb_client = boto3.client('dynamodb', region_name='us-east-1')
 
 # Attempt to create the global UserSearchTable
 try:
@@ -24,17 +24,44 @@ try:
             'StreamViewType': 'NEW_AND_OLD_IMAGES'
         }
     )
+except dynamodb_client.exceptions.ResourceInUseException:
+    pass # do nothing, table exists
 
-    response = dynamodb_client.create_global_table(
-        GlobalTableName='UserSearchTable',
-        ReplicationGroup=[
+dynamodb_client = boto3.client('dynamodb', region_name='us-west-1')
+
+# Attempt to create the global UserSearchTable
+try:
+    response = dynamodb_client.create_table(
+        TableName='UserSearchTable',
+        AttributeDefinitions=[
             {
-                'RegionName': 'us-east-1'
-            },
+                'AttributeName': 'id',
+                'AttributeType': 'S'
+            }
+        ],
+        KeySchema=[
             {
-                'RegionName': 'us-west-1'
-            },
-        ]
+                'AttributeName': 'id',
+                'KeyType': 'HASH'
+            }
+        ],
+        BillingMode='PAY_PER_REQUEST',
+        StreamSpecification={
+            'StreamEnabled': True,
+            'StreamViewType': 'NEW_AND_OLD_IMAGES'
+        }
     )
 except dynamodb_client.exceptions.ResourceInUseException:
     pass # do nothing, table exists
+
+response = dynamodb_client.create_global_table(
+    GlobalTableName='UserSearchTable',
+    ReplicationGroup=[
+        {
+            'RegionName': 'us-east-1'
+        },
+        {
+            'RegionName': 'us-west-1'
+        },
+    ]
+)
